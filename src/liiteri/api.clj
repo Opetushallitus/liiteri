7b9c046830a0831ee9de96fe2a13d6289a08a786
Esko Lahti <esko.lahti@reaktor.com>
2017-02-15 16:14:03 +0200

    Disable spec validation

--- src/liiteri/api.clj
+++ src/liiteri/api.clj
@@ -2,9 +2,10 @@
   (:require [compojure.api.sweet :as api]))
 
 (defn new-api []
-  (api/api {:swagger {:spec "/liiteri/swagger.json"
-                      :ui   "/liiteri/api-docs"
-                      :data {:info {:version     "0.1.0"
-                                    :title       "Liiteri API"
-                                    :description "File Storage Service API For OPH"}}}}
+  (api/api {:swagger {:spec         "/liiteri/swagger.json"
+                      :ui           "/liiteri/api-docs"
+                      :validatorUrl nil
+                      :data         {:info {:version     "0.1.0"
+                                            :title       "Liiteri API"
+                                            :description "File Storage Service API For OPH"}}}}
     (api/context "/liiteri" [])))
