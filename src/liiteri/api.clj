809211a4a0bb7e941f3afe231270ebbac55a4c5e
Esko Lahti <esko.lahti@reaktor.com>
2017-02-15 15:06:03 +0200

    Initial Swagger docs

--- src/liiteri/api.clj
+++ src/liiteri/api.clj
@@ -1,4 +1,10 @@
-(ns liiteri.api)
+(ns liiteri.api
+  (:require [compojure.api.sweet :as api]))
 
 (defn new-api []
-  )
+  (api/api {:swagger {:spec "/liiteri/swagger.json"
+                      :ui   "/liiteri/api-docs"
+                      :data {:info {:version     "0.1.0"
+                                    :title       "Liiteri API"
+                                    :description "File Storage Service API For OPH"}}}}
+    (api/context "/liiteri" [])))
