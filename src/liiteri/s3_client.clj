0d5205c081ad00642876d3e42426def6217db29b
hprajani <hannu-pekka.rajaniemi@helsinki.fi>
2017-03-15 13:16:40 +0200

    more consistent way of doing things

--- src/liiteri/s3_client.clj
+++ src/liiteri/s3_client.clj
@@ -12,9 +12,9 @@
                      (.withRegion Regions/EU_CENTRAL_1)
                      (.withCredentials (SystemPropertiesCredentialsProvider.))
                      (.build))
-          bucket ()]
+          bucket (System/getProperty "aws.bucket" "liiteri-dev")]
       (assoc this :s3-client client)
-      (assoc this :s3-bucket (System/getProperty "aws.bucket" "liiteri-dev"))))
+      (assoc this :s3-bucket bucket)))
 
   (stop [this]
     (when-let [client (:s3-client this)]
