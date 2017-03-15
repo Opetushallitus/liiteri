c6742d3a84bb47c648acc4b9a815d0b88e3424e1
hprajani <hannu-pekka.rajaniemi@helsinki.fi>
2017-03-15 13:06:46 +0200

    s3 bucket from settings

--- src/liiteri/s3_store.clj
+++ src/liiteri/s3_store.clj
@@ -8,7 +8,7 @@
 
 (defn create-file [file s3-client db]
   (let [key       (str (UUID/randomUUID))
-        s3-object (.putObject (:s3-client s3-client) "hpr-liiteri" key (:tempfile file))]
+        s3-object (.putObject (:s3-client s3-client) (:s3-bucket s3-client) key (:tempfile file))]
     (jdbc/with-db-transaction [datasource db]
       (let [conn {:connection datasource}]
         (file-store/create-file (merge (select-keys file [:filename :content-type :size])
@@ -24,12 +24,12 @@
     (let [conn              {:connection datasource}
           previous-versions (file-store/get-file-for-update key conn)]
       (when (every? not-deleted? previous-versions)
-        (let [s3-object (.putObject (:s3-client s3-client) "hpr-liiteri" key (:tempfile file))]
+        (let [s3-object (.putObject (:s3-client s3-client) (:s3-bucket s3-client) key (:tempfile file))]
           (file-store/create-file (merge (select-keys file [:filename :content-type :size])
                                          {:key key :version (.getVersionId s3-object)})
                                   conn))))))
 (defn get-file-stream [key s3-client]
-  (let [file-object (.getObject (:s3-client s3-client) "hpr-liiteri" key)]
+  (let [file-object (.getObject (:s3-client s3-client) (:s3-bucket s3-client) key)]
     (.getObjectContent file-object)))
 
 (defn delete-file [key s3-client db]
