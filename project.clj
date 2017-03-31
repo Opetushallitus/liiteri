23cc28cf59665d66aa3d6dd8d34b7ba22fd09680
Esko Lahti <esko.lahti@reaktor.com>
2017-03-31 09:45:34 +0300

    Updated schema

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                  [http-kit "2.2.0"]
                  [org.clojure/java.jdbc "0.7.0-alpha1"]
                  [org.postgresql/postgresql "42.0.0"]
-                 [prismatic/schema "1.1.3"]
+                 [prismatic/schema "1.1.4"]
                  [metosin/schema-tools "0.9.0"]
                  [javax.servlet/servlet-api "2.5"]
                  [com.taoensso/timbre "4.8.0"]
