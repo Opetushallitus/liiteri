f0afc11509b4725642cf62f8aff046d4aed08659
Esko Lahti <esko.lahti@reaktor.com>
2017-04-07 15:13:02 +0300

    Initialize temp file only once

--- test/liiteri/virus_scan_test.clj
+++ test/liiteri/virus_scan_test.clj
@@ -21,42 +21,51 @@
 (defn- stop-system []
   (component/stop-system @system-state))
 
-(defn clear-database! []
+(defn- clear-database! []
   (let [datasource (-> (:db @system-state)
                        (select-keys [:datasource]))]
     (jdbc/db-do-commands datasource ["DROP SCHEMA IF EXISTS public CASCADE"
                                      "CREATE SCHEMA public"])))
 
+(defn- temp-dir []
+  (-> (get-in (:config @system-state) [:file-store :filesystem :base-path])
+      (io/file)))
+
+(defn- create-temp-dir []
+  (.mkdirs (temp-dir)))
+
 (defn- init-test-file []
   (jdbc/with-db-transaction [datasource (:db @system-state)]
-    (let [filename (str (UUID/randomUUID))
+    (let [filename "test-file.txt"
           file-key (str (UUID/randomUUID))
-          conn     {:connection datasource}]
+          conn     {:connection datasource}
+          base-dir (get-in (:config @system-state) [:file-store :filesystem :base-path])]
+      (with-open [w (io/writer (str base-dir "/" file-key))]
+        (.write w "test file\n"))
       (reset! metadata (metadata-store/create-file {:key          file-key
-                                                    :filename     (str filename ".txt")
+                                                    :filename     filename
                                                     :content-type "text/plain"
                                                     :size         1}
                                                    conn))
-      (reset! file (File/createTempFile filename "txt")))))
+      (reset! file (io/file (str base-dir "/" file-key))))))
 
-(defn- remove-test-file []
-  (metadata-store/delete-file (:key @metadata) (:db @system-state))
-  (io/delete-file @file))
+(defn- remove-temp-dir []
+  (letfn [(remove-node [node]
+            (when (.isDirectory node)
+              (doseq [child-node (.listFiles node)]
+                (remove-node child-node)))
+            (io/delete-file node))]
+    (remove-node (temp-dir))))
 
 (use-fixtures :once
   (fn [tests]
     (start-system)
+    (create-temp-dir)
+    (init-test-file)
     (tests)
     (clear-database!)
-    (stop-system)))
-
-(use-fixtures :each
-  (fn [tests]
-    (init-test-file)
-    (try
-      (tests)
-      (finally
-        (remove-test-file)))))
+    (stop-system)
+    (remove-temp-dir)))
 
 (deftest virus-scan-for-clean-file
   (let [db             (:db @system-state)
