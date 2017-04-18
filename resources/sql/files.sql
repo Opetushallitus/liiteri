d9346e3b7c6f70b8c57b6cf04b7d83014e1b2723
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2017-04-18 16:05:27 +0300

    Show deleted file metadata to virkailija

--- resources/sql/files.sql
+++ resources/sql/files.sql
@@ -6,9 +6,11 @@ UPDATE files SET deleted = NOW() WHERE key = :key AND deleted IS NULL;
 
 -- name: sql-get-metadata
 SELECT key, filename, content_type, size, uploaded, deleted, virus_scan_status
-  FROM files
-  WHERE key IN (:keys)
-  AND (deleted IS NULL OR deleted > NOW());
+FROM files
+WHERE key IN (:keys)
+  AND deleted IS NULL
+  OR deleted > NOW()
+  OR virus_scan_status = 'failed';
 
 -- name: sql-get-unscanned-file
 SELECT key, filename, content_type FROM files WHERE virus_scan_status = 'not_started' LIMIT 1 FOR UPDATE SKIP LOCKED;
