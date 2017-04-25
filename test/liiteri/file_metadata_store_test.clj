7994e7940f7c26d6a11c52d33c4e148daa157b47
Esko Lahti <esko.lahti@reaktor.com>
2017-04-25 13:05:46 +0300

    Verify that multiple invalid characters are removed

--- test/liiteri/file_metadata_store_test.clj
+++ test/liiteri/file_metadata_store_test.clj
@@ -8,6 +8,6 @@
                                ["parrot/:;.png" "parrot.png"]
                                ["p a r r o t . p n g" "p a r r o t . p n g"]
                                ["paRRot1234.png" "paRRot1234.png"]
-                               ["pa:-_rrot.png" "pa-_rrot.png"]]]
+                               ["pa::::-_rrot.png" "pa-_rrot.png"]]]
     (let [actual (#'metadata-store/normalize filename)]
       (is (= expected actual)))))
