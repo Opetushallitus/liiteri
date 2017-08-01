f4b142b880acb71a647396b293c90244d11daaae
Esko Lahti <esko.lahti@reaktor.com>
2017-08-01 15:55:45 +0300

    org.clojure/java.jdbc update

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [fi.vm.sade/auditlogger "5.0.0-SNAPSHOT"]
                  [hikari-cp "1.7.6"]
                  [http-kit "2.2.0"]
-                 [org.clojure/java.jdbc "0.7.0-alpha1"]
+                 [org.clojure/java.jdbc "0.7.0"]
                  [org.postgresql/postgresql "42.0.0"]
                  [prismatic/schema "1.1.5"]
                  [metosin/schema-tools "0.9.0"]
