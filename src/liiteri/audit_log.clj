7a568461a6e3fd94745c75cc89e8fcfc93e86454
Jani Rahkola <jani.rahkola@reaktor.com>
2018-02-08 17:03:42 +0200

    Käytä auditlogger 8.0.0-SNAPSHOTtia, timbre logituksella

--- src/liiteri/audit_log.clj
+++ src/liiteri/audit_log.clj
@@ -1,41 +1,87 @@
 (ns liiteri.audit-log
-  (:require [clj-time.core :as t]
-            [clj-time.format :as f]
-            [cheshire.core :as json]
-            [com.stuartsierra.component :as component])
-  (:import [fi.vm.sade.auditlog Audit ApplicationType CommonLogMessageFields AbstractLogMessage]))
+  (:require [com.stuartsierra.component :as component]
+            [taoensso.timbre :as timbre]
+            [taoensso.timbre.appenders.3rd-party.rolling :refer [rolling-appender]])
+  (:import [fi.vm.sade.auditlog Audit ApplicationType Changes$Builder Logger Operation Target$Builder User]
+           java.net.InetAddress))
 
-(def ^:private date-time-formatter (f/formatter :date-time))
+(deftype New []
+  Operation
+  (name [_] "lisäys"))
 
-(def operation-new "lisäys")
-(def operation-finalize "vahvistus")
-(def operation-delete "poisto")
-(def operation-query "haku")
+(deftype Finalize []
+  Operation
+  (name [_] "vahvistus"))
+
+(deftype Delete []
+  Operation
+  (name [_] "poisto"))
+
+(deftype FileQuery []
+  Operation
+  (name [_] "tiedosto haku"))
+
+(deftype MetadataQuery []
+  Operation
+  (name [_] "metatieto haku"))
+
+(def operation-new (->New))
+(def operation-finalize (->Finalize))
+(def operation-delete (->Delete))
+(def operation-file-query (->FileQuery))
+(def operation-metadata-query (->MetadataQuery))
+
+(defn file-target [key]
+  (-> (new Target$Builder)
+      (.setField "file-key" key)
+      .build))
+
+(defn new-file-changes [file]
+  (-> (new Changes$Builder)
+      (.added "size" (str (:size file)))
+      (.added "uploaded" (str (:uploaded file)))
+      (.added "filename" (:filename file))
+      (.added "content-type" (:content-type file))
+      (.build)))
+
+(def no-changes (.build (new Changes$Builder)))
+
+(defn unknown-user [ip-str user-agent]
+  (new User (InetAddress/getByName ip-str) "" user-agent))
+
+(deftype LoggerAdapter [log-config]
+  Logger
+  (log [_ str]
+    (timbre/log* log-config :info str)))
+
+(defn- audit-log-config [log-path]
+  (assoc timbre/example-config
+         :appenders {:file-appender
+                     (assoc (rolling-appender {:path log-path
+                                               :pattern :daily})
+                            :output-fn (fn [{:keys [msg_]}] (force msg_)))}))
 
 (defprotocol AuditLog
-  (log [this id operation message]))
+  (log [this user operation target changes]))
 
-(defrecord AuditLogger []
+(defrecord AuditLogger [config logger]
   component/Lifecycle
 
   (start [this]
-    (assoc this :logger (Audit. "liiteri" ApplicationType/BACKEND)))
+    (let [log-path (get-in config [:audit-log :path])]
+      (when (clojure.string/blank? log-path)
+        (throw (new RuntimeException "Invalid empty audit log path")))
+      (assoc this :logger (Audit. (->LoggerAdapter (audit-log-config log-path))
+                                  "liiteri"
+                                  ApplicationType/BACKEND))))
 
   (stop [this]
     (assoc this :logger nil))
 
   AuditLog
 
-  (log [this id operation message]
-    (let [logger    (:logger this)
-          timestamp (f/unparse date-time-formatter (t/now))
-          id-map    {:file-keys (if (string? id) [id] id)}
-          log-map   {CommonLogMessageFields/ID        (json/generate-string id-map)
-                     CommonLogMessageFields/TIMESTAMP timestamp
-                     CommonLogMessageFields/OPERAATIO operation
-                     CommonLogMessageFields/MESSAGE   (json/generate-string message)}]
-      (->> (proxy [AbstractLogMessage] [log-map])
-           (.log logger)))))
+  (log [this user operation target changes]
+    (.log logger user operation target changes)))
 
 (defn new-logger []
   (map->AuditLogger {}))
