1a74bb9365a844134be74dd79e6c7c9b02f9ecf8
Harri Salokorpi <harri.salokorpi@reaktor.com>
2019-10-08 20:32:29 +0300

    Extract file fixtures to fixtures.clj

---
+++ test/liiteri/fixtures.clj
@@ -0,0 +1,34 @@
+(ns liiteri.fixtures
+  (:require [clojure.java.io :as io]))
+
+(defn load-test-file [filename content-type]
+  (let [file-object (io/file (io/resource (format "test-files/%s" filename)))]
+    [filename file-object content-type]))
+
+(def file-types
+  {:exe "application/octet-stream"
+   :txt "text/plain"
+   :jpg "image/jpeg"
+   :png "image/png"
+   :gif "image/gif"
+   :rtf "application/rtf"
+   :pdf "application/pdf"
+   :odt "application/vnd.oasis.opendocument.text"
+   :doc "application/msword"
+   :docx "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+   :ods "application/vnd.oasis.opendocument.spreadsheet"
+   :xls "application/vnd.ms-excel"
+   :xlsx "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
+
+(def forbidden-files [(load-test-file "sample.exe" (:exe file-types))])
+
+(def ok-files [(load-test-file "sample.doc" (:doc file-types))
+               (load-test-file "sample.docx" (:docx file-types))
+               (load-test-file "sample.jpg" (:jpg file-types))
+               (load-test-file "sample.ods" (:ods file-types))
+               (load-test-file "sample.odt" (:odt file-types))
+               (load-test-file "sample.pdf" (:pdf file-types))
+               (load-test-file "sample.png" (:png file-types))
+               (load-test-file "sample.txt" (:txt file-types))
+               (load-test-file "sample.xls" (:xls file-types))
+               (load-test-file "sample.xlsx" (:xlsx file-types))])
--- test/liiteri/mime_test.clj
+++ test/liiteri/mime_test.clj
@@ -1,44 +1,11 @@
 (ns liiteri.mime-test
   (:require [liiteri.mime :as mime]
             [liiteri.config :as config]
-            [clojure.java.io :as io]
+            [liiteri.fixtures :refer :all]
             [clojure.test :refer :all]
-            [taoensso.timbre :as log]
-            ))
+            [taoensso.timbre :as log]))
 
 (def config (config/new-config))
-
-(defn load-test-file [filename content-type]
-  (let [file-object (io/file (io/resource (format "test-files/%s" filename)))]
-    [filename file-object content-type]))
-
-(def types
-  {:exe "application/octet-stream"
-   :txt "text/plain"
-   :jpg "image/jpeg"
-   :png "image/png"
-   :gif "image/gif"
-   :rtf "application/rtf"
-   :pdf "application/pdf"
-   :odt "application/vnd.oasis.opendocument.text"
-   :doc "application/msword"
-   :docx "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-   :ods "application/vnd.oasis.opendocument.spreadsheet"
-   :xls "application/vnd.ms-excel"
-   :xlsx "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
-
-(def forbidden-files [(load-test-file "sample.exe" (:exe types))])
-
-(def ok-files [(load-test-file "sample.doc" (:doc types))
-               (load-test-file "sample.docx" (:docx types))
-               (load-test-file "sample.jpg" (:jpg types))
-               (load-test-file "sample.ods" (:ods types))
-               (load-test-file "sample.odt" (:odt types))
-               (load-test-file "sample.pdf" (:pdf types))
-               (load-test-file "sample.png" (:png types))
-               (load-test-file "sample.txt" (:txt types))
-               (load-test-file "sample.xls" (:xls types))
-               (load-test-file "sample.xlsx" (:xlsx types))])
 
 (deftest ok-mime-type-allowed
   (doseq [[name file content-type] ok-files]
