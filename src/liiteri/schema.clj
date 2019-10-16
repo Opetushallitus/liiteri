5cf917ebbedc52034722654186738416c6e707cf
Harri Salokorpi <harri.salokorpi@reaktor.com>
2019-10-16 15:28:06 +0300

    Introduce API schema definition for preview data

--- src/liiteri/schema.clj
+++ src/liiteri/schema.clj
@@ -5,12 +5,22 @@
 
 ;; This is the public schema of Liiteri API
 
+(s/defschema Preview
+  {:key                       s/Str
+   :content-type              s/Str
+   :size                      s/Int
+   (s/optional-key :uploaded) DateTime
+   (s/optional-key :deleted)  (s/maybe DateTime)})
+
 (s/defschema File
   {:key                       s/Str
    :filename                  s/Str
    :content-type              s/Str
    :size                      s/Int
+   :page-count                (s/maybe s/Int)
    :virus-scan-status         s/Str
    :final                     s/Bool
    (s/optional-key :uploaded) DateTime
-   (s/optional-key :deleted)  (s/maybe DateTime)})
+   (s/optional-key :deleted)  (s/maybe DateTime)
+   :preview-status            (s/enum "not_supported" "not_generated" "finished")
+   (s/optional-key :previews)  [Preview]})
