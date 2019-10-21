c192a893d1a7825f90c123f88c2f286d0679ddda
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-10-21 13:53:40 +0300

    Introduce preview tables and columns to database

--- resources/migrations/V1_5__add_preview_tables.sql
+++ resources/migrations/V1_5__add_preview_tables.sql
@@ -1,4 +1,4 @@
-CREATE TYPE preview_generation_status AS ENUM ('not_supported', 'not_generated', 'finished');
+CREATE TYPE preview_generation_status AS ENUM ('not_supported', 'not_generated', 'finished', 'error');
 
 ALTER TABLE files ADD COLUMN page_count INTEGER;
 ALTER TABLE files ADD COLUMN preview_status preview_generation_status NOT NULL DEFAULT 'not_generated';
@@ -7,11 +7,14 @@ CREATE TABLE previews (
   file_id BIGSERIAL REFERENCES files(id) NOT NULL,
   page_number INTEGER NOT NULL,
 
-  key VARCHAR(36) UNIQUE NOT NULL,
+  key VARCHAR(64) UNIQUE NOT NULL,
+  filename VARCHAR(256) NOT NULL,
   content_type VARCHAR(128) NOT NULL DEFAULT 'image/png',
   size BIGINT NOT NULL,
   uploaded TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
   deleted TIMESTAMP WITH TIME ZONE,
+
+  final BOOLEAN NOT NULL DEFAULT FALSE,
 
   UNIQUE(file_id, page_number)
 );
