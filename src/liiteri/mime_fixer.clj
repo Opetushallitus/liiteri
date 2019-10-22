cdf3181eef467552fc78309f853de47e86f2742a
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-10-22 09:09:03 +0300

    HLE-1036 : Hiljennä logitusta
    Jos mitään prosessoitavaa ei löydy useamman kerran peräkkäin,
    ei logiteta.

--- src/liiteri/mime_fixer.clj
+++ src/liiteri/mime_fixer.clj
@@ -11,6 +11,8 @@
 
 (def mime-type-for-failed-cases "application/octet-stream")
 
+(def were-unprocessed-files-found-on-last-run (atom true))
+
 (defn- log-mime-type-fix-result [file-key filename content-type status elapsed-time]
   (let [status-str (if (= status :successful) "OK" "FAILED")]
     (log/info (str "Mime type fix took " elapsed-time " ms, status " status-str " for file " filename " with key " file-key " (" content-type ")"))))
@@ -53,9 +55,13 @@
     (jdbc/with-db-transaction [tx db]
                               (let [conn {:connection tx}]
                                 (if-let [file (metadata-store/get-file-without-mime-type conn)]
-                                  (fix-mime-type-of-file conn storage-engine file)
                                   (do
-                                    (log/info "MIME type fixing seems to be finished (or errored).")
+                                    (reset! were-unprocessed-files-found-on-last-run true)
+                                    (fix-mime-type-of-file conn storage-engine file))
+                                  (do
+                                    (when @were-unprocessed-files-found-on-last-run
+                                      (log/info "MIME type fixing seems to be finished (or errored)."))
+                                    (reset! were-unprocessed-files-found-on-last-run false)
                                     false))))
     (catch Exception e
       (log/error e "Failed to fix mime type of the next file")
