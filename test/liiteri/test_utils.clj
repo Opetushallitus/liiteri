9114a72582279329f673c787e676c198f0255209
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-10-23 09:07:57 +0300

    HLE-1036 : Siirrä koodi uudelleenkäytettävään paikkaan
    Ei toiminnallisia muutoksia.

--- test/liiteri/test_utils.clj
+++ test/liiteri/test_utils.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.jdbc :as jdbc]
             [com.stuartsierra.component :as component]
             [liiteri.core :as system]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [liiteri.files.file-store :as file-store]))
 
 (defn start-system [system]
   (let [s (or @system (system/new-system))]
@@ -31,3 +32,20 @@
                 (remove-node child-node)))
             (io/delete-file node))]
     (remove-node (temp-dir system))))
+
+(def files-atom (atom {}))
+
+(defrecord InMemoryS3Store [config]
+  file-store/StorageEngine
+
+  (create-file [this file file-key]
+    (swap! files-atom assoc file-key file))
+
+  (delete-file [this file-key]
+    (swap! files-atom dissoc file-key))
+
+  (get-file [this file-key]
+    (io/input-stream (get @files-atom file-key))))
+
+(defn new-in-memory-store []
+  (map->InMemoryS3Store {}))
