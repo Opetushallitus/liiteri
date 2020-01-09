109c67046254c9db36b4e7c17abd39a6f29b0911
Harri Salokorpi <harri.salokorpi@reaktor.com>
2020-01-09 15:05:24 +0200

    HLE-1128: Lisää testausta retry-logiikalle

--- dev-resources/sql/test-files.sql
+++ dev-resources/sql/test-files.sql
@@ -1,5 +1,5 @@
 -- name: sql-get-metadata-for-tests
-SELECT key, filename, content_type, size, uploaded, deleted, virus_scan_status, final
+SELECT key, filename, content_type, size, uploaded, deleted, virus_scan_status, virus_scan_retry_count, final
   FROM files
   WHERE key IN (:keys);
 
