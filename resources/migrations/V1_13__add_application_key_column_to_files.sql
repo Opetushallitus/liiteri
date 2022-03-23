b1a22aaab595b21c870d2aa2de8240cc1a712eb4
Tatu Honkanen <tatu.honkanen@cybercom.com>
2022-03-23 12:13:38 +0200

    OY-3245 : Default to application-key column.

--- resources/migrations/V1_13__add_application_key_column_to_files.sql
+++ resources/migrations/V1_13__add_application_key_column_to_files.sql
@@ -1,1 +1,1 @@
-alter table files add column if not exists application_key varchar(40) not null;
+alter table files add column if not exists application_key varchar(40) not null default 'TODO application-key';
